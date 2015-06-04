8d61098b43968764143fdd17bd3142feb34c867f
unknown <paul.kiernan@kk-laptop.(none)>
2015-06-05 00:37:11 +0100

    Added new integration test that fails need to fix

---
+++ Handsey.Tests.Integration/Handlers/ModelMapperHandler.cs
@@ -0,0 +1,22 @@
+﻿using Handsey.Tests.Integration.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Handsey.Tests.Integration.Models;
+
+namespace Handsey.Tests.Integration.Handlers
+{
+    public class ModelMapperHandler<TFrom, TTo>
+        : IOneToOneHandler<TFrom, TTo>
+        where TFrom : IVerifiable
+        where TTo : IVerifiable
+    {
+        public void Handle(TFrom arg1, TTo arg2)
+        {
+            arg1.UpdateLog(this);
+            arg2.UpdateLog(this);
+        }
+    }
+}
--- Handsey.Tests.Integration/MainTest.cs
+++ Handsey.Tests.Integration/MainTest.cs
@@ -38,7 +38,7 @@ namespace Handsey.Tests.Integration
 
         [TestCase(10000, false)]
         [TestCase(10000, true)]
-        public void InvokeHandlersForIterations(int iterations, bool clearRegistraionsCache)
+        public void ThreadingTest(int iterations, bool clearRegistraionsCache)
         {
             _stopwatch.Restart();
 
@@ -46,31 +46,13 @@ namespace Handsey.Tests.Integration
             Parallel.For(0, iterations, (i) =>
             {
                 // create a new domain object
-                Developer developer = new Developer("paul", "kiernan", new string[] { "C#" });
+                TriggerChangeOnADeveloper();
 
-                // change it
-                developer.Change(developer.FirstName, developer.LastName, new string[] { "C#", "JS" });
+                TriggerChangeOnAnEmployee();
 
-                // this won't work in a parralle FIX
-                IHandler[] developerHandlerLog = developer.HandlerLog();
-                Assert.That(developerHandlerLog.Count(), Is.EqualTo(3));
-                Assert.That(developerHandlerLog.Count(h => h.GetType() == typeof(AlertStateChangeHandler<Developer>)), Is.EqualTo(1));
-                Assert.That(developerHandlerLog.Count(h => h.GetType() == typeof(SaveStateChangeHandler<Developer>)), Is.EqualTo(1));
-                Assert.That(developerHandlerLog.Count(h => h.GetType() == typeof(ProgrammingLanguagesChangedHandler<Developer>)), Is.EqualTo(1));
+                MapDeveloperViewModelToDevelopViewModel();
 
-                // create a new domain object
-                Employee employee = new Employee("paul", "kiernan");
-
-                // change it
-                employee.Change(employee.FirstName, "Mc Kiernan");
-
-                //// this won't work in a parralle FIX
-                IHandler[] employeeHandlerLog = employee.HandlerLog();
-                Assert.That(employeeHandlerLog.Count(), Is.EqualTo(2));
-                Assert.That(employeeHandlerLog.Count(h => h.GetType() == typeof(AlertStateChangeHandler<Employee>)), Is.EqualTo(1));
-                Assert.That(employeeHandlerLog.Count(h => h.GetType() == typeof(SaveStateChangeHandler<Employee>)), Is.EqualTo(1));
-
-                InvokeMappingHandler<DeveloperViewModel, DeveloperViewModel>();
+                MapTechnicalArchitectToTechnicalArchitectViewModel();
 
                 // optionally clear the cache
                 if (clearRegistraionsCache)
@@ -81,30 +63,95 @@ namespace Handsey.Tests.Integration
             Console.WriteLine("InvokeHandlersForIterations iterations {0}, clearRegistraionsCache {1}, took {2} milliseconds / {3} ticks", iterations, clearRegistraionsCache, _stopwatch.ElapsedMilliseconds, _stopwatch.ElapsedTicks);
         }
 
-        public void InvokeMappingHandler<TFrom, TTo>()
+        [Test]
+        public void TriggerChangeOnADeveloper()
+        {
+            Developer developer = new Developer("paul", "kiernan", new string[] { "C#" });
+
+            // change it
+            developer.Change(developer.FirstName, developer.LastName, new string[] { "C#", "JS" });
+
+            // this won't work in a parralle FIX
+            IHandler[] handlerLog = developer.HandlerLog();
+            Assert.That(handlerLog.Count(), Is.EqualTo(3));
+            Assert.That(handlerLog.Count(h => h.GetType() == typeof(AlertStateChangeHandler<Developer>)), Is.EqualTo(1));
+            Assert.That(handlerLog.Count(h => h.GetType() == typeof(SaveStateChangeHandler<Developer>)), Is.EqualTo(1));
+            Assert.That(handlerLog.Count(h => h.GetType() == typeof(ProgrammingLanguagesChangedHandler<Developer>)), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TriggerChangeOnAnEmployee()
+        {
+            // create a new domain object
+            Employee employee = new Employee("paul", "kiernan");
+
+            // change it
+            employee.Change(employee.FirstName, "Mc Kiernan");
+
+            //// this won't work in a parralle FIX
+            IHandler[] handlerLog = employee.HandlerLog();
+            Assert.That(handlerLog.Count(), Is.EqualTo(2));
+            Assert.That(handlerLog.Count(h => h.GetType() == typeof(AlertStateChangeHandler<Employee>)), Is.EqualTo(1));
+            Assert.That(handlerLog.Count(h => h.GetType() == typeof(SaveStateChangeHandler<Employee>)), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void MapDeveloperViewModelToDevelopViewModel()
+        {
+            // map to a view model
+            DeveloperViewModel developerFrom = new DeveloperViewModel();
+            DeveloperViewModel developerTo = new DeveloperViewModel();
+
+            InvokeUpdateRequestMappingHandler<DeveloperViewModel, DeveloperViewModel>(developerFrom, developerTo);
+
+            IHandler[] fromHandlerLog = developerFrom.HandlerLog();
+            IHandler[] toHandlerLog = developerTo.HandlerLog();
+
+            Assert.That(fromHandlerLog.Count(), Is.EqualTo(1));
+            Assert.That(fromHandlerLog.Count(h => h.GetType() == typeof(UpdateRequestHandler<DeveloperViewModel, DeveloperViewModel>)), Is.EqualTo(1));
+
+            Assert.That(toHandlerLog.Count(), Is.EqualTo(1));
+            Assert.That(toHandlerLog.Count(h => h.GetType() == typeof(UpdateRequestHandler<DeveloperViewModel, DeveloperViewModel>)), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void MapTechnicalArchitectToTechnicalArchitectViewModel()
+        {
+            // map to a view model
+            TechnicalArchitect from = new TechnicalArchitect("Len", "Boyde", new[] { "JS", "JAVA", "C#" });
+            TechnicalArchitectViewModel to = new TechnicalArchitectViewModel();
+
+            InvokeMappingHandler<TechnicalArchitect, TechnicalArchitectViewModel>(from, to);
+
+            IHandler[] fromHandlerLog = from.HandlerLog();
+            IHandler[] toHandlerLog = to.HandlerLog();
+
+            Assert.That(fromHandlerLog.Count(), Is.EqualTo(1));
+            Assert.That(fromHandlerLog.Count(h => h.GetType() == typeof(ModelMapperHandler<TechnicalArchitect, TechnicalArchitectViewModel>)), Is.EqualTo(1));
+
+            Assert.That(toHandlerLog.Count(), Is.EqualTo(1));
+            Assert.That(toHandlerLog.Count(h => h.GetType() == typeof(ModelMapperHandler<TechnicalArchitect, TechnicalArchitectViewModel>)), Is.EqualTo(1));
+        }
+
+        public void InvokeMappingHandler<TFrom, TTo>(TFrom from, TTo to)
+        {
+            // invoke
+            ApplicationLocator
+                .Instance
+                .Invoke<IOneToOneHandler<TFrom, TTo>>(h => h.Handle(from, to));
+        }
+
+        public void InvokeUpdateRequestMappingHandler<TFrom, TTo>(TFrom from, TTo to)
             where TFrom : EmployeeViewModel, new()
             where TTo : EmployeeViewModel, new()
         {
-            // map to a view model
-            TFrom developerFrom = new TFrom();
-            TTo developerTo = new TTo();
-
             // invoke
             ApplicationLocator
                 .Instance
                 .Invoke<IOneToOneHandler<UpdateEmployeeRequest<TFrom>, UpdateEmployeeResponse<TTo>>>(
-                h => h.Handle(new UpdateEmployeeRequest<TFrom>() { Employee = developerFrom }
-                    , new UpdateEmployeeResponse<TTo>() { Employee = developerTo }
+                h => h.Handle(new UpdateEmployeeRequest<TFrom>() { Employee = from }
+                    , new UpdateEmployeeResponse<TTo>() { Employee = to }
                     ));
-
-            IHandler[] developerFromHandlerLog = developerFrom.HandlerLog();
-            IHandler[] developerToHandlerLog = developerTo.HandlerLog();
-
-            Assert.That(developerFromHandlerLog.Count(), Is.EqualTo(1));
-            Assert.That(developerFromHandlerLog.Count(h => h.GetType() == typeof(UpdateRequestHandler<TFrom, TTo>)), Is.EqualTo(1));
-
-            Assert.That(developerToHandlerLog.Count(), Is.EqualTo(1));
-            Assert.That(developerToHandlerLog.Count(h => h.GetType() == typeof(UpdateRequestHandler<TFrom, TTo>)), Is.EqualTo(1));
         }
     }
 }
