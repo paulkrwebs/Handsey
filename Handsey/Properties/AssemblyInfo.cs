20ad190174346aab530a390ca80cb56f7ab4a7b8
Paul Kiernan <paulk@rwebs.co.uk>
2016-02-07 17:57:42 +0000

    adding assembly properties

--- Handsey/Properties/AssemblyInfo.cs
+++ Handsey/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Handsey")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Handsey, the automatic event handler registration framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Paul Kiernan")]
 [assembly: AssemblyProduct("Handsey")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
